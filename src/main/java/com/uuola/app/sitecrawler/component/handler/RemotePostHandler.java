7f5b1bcea9ea55bd129d94bf40ba8e4a8e9298b3
tangxiaodong <tangxiaodong@NH03ITNB7000675.yihaodian.com>
2016-04-05 22:24:57 +0800

    update

--- src/main/java/com/uuola/app/sitecrawler/component/handler/RemotePostHandler.java
+++ src/main/java/com/uuola/app/sitecrawler/component/handler/RemotePostHandler.java
@@ -41,6 +41,7 @@ public class RemotePostHandler {
         MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
         headers.setContentType(type);
         headers.add("Accept", MediaType.APPLICATION_JSON.toString());
+        headers.add("AccessToken", "asdfjklasdkfl");
         try {
             HttpEntity<String> formEntity = new HttpEntity<String>(JsonUtil.toJSONString(rec), headers);
             String response = restTemplate.postForObject(Config.RECORD_POST_URL, formEntity, String.class);
