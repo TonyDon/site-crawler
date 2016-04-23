474e3f8145642bc0159e52396b9d2ba8473a73e5
tangxiaodong <tangxiaodong@NH03ITNB7000675.yihaodian.com>
2016-04-23 14:35:34 +0800

    update

--- src/main/java/com/uuola/app/sitecrawler/constants/Config.java
+++ src/main/java/com/uuola/app/sitecrawler/constants/Config.java
@@ -20,11 +20,11 @@ public interface Config {
     
     public final String RECORD_POST_URL = "http://m.986001.com/spi/record/post.json";
     
-    public final int UPLOAD_MAX_FILE_SIZE = 1024*1024 ;
+    public final int UPLOAD_MAX_FILE_SIZE = 1560576 ;
     
     public final int IMG_MAX_WIDTH = 650;
     
-    public final long NEED_FIX_MAX_SIZE = 500000;
+    public final long NEED_FIX_MAX_SIZE = 512000;
     
     public final int IMG_FIX_WIDTH = 650;
     
